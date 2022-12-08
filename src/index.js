7e53c67cf092b99e72f4c008c9145b19870abf41
hjsad1994 <trantai306@gmail.com>
2022-12-08 17:33:18 +0700

    new2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-const express = require('express') // 
-const path = require('path')
+const express = require('express') 
+const path = require('path') // import path
 const morgan = require('morgan') // import morgan 
-const {engine} = require('express-handlebars')
+const {engine} = require('express-handlebars') // import express-handlebars
 
 const app = express()
 const port = 3000
 // using morgan
 app.use(morgan('combined'))
 // using express-handlebars
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'resources/views'))
+app.engine('hbs', engine({
+  extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views')) // set local file
 
 app.get('/', (req, res) => {
   res.render('home')
